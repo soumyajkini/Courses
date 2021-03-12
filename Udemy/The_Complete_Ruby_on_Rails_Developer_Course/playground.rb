7dfb1479123d68b8308c408d3203751276064cd4
Soumya Kini-Shivakumar <soumya@Soumyas-MacBook-Pro.local>
2021-03-12 23:28:31 +0530

    Course KickOff

---
+++ Udemy/The_Complete_Ruby_on_Rails_Developer_Course/playground.rb
@@ -0,0 +1,19 @@
+puts "Hello World!"
+
+#Using variables
+hello = "Hello World!"
+puts hello
+
+#Using methods
+def say_hello
+  puts "Hello World!"
+end
+
+#Using methods with params
+def say_hello_params(thing_to_say)
+  puts thing_to_say
+end
+
+say_hello
+
+say_hello_params "Hello Everyone !!"
